f341f951b8bf58408a7784079333ece3191d3f0d
Alfadjri Dwi Fadhilah <alfadjri28@gmail.com>
2025-12-26 16:36:31 +0700

    Add Latihan hari 3 detail

--- app/src/main/java/com/codingstudio/bookcatalog/asset/StoryAdapter.java
+++ app/src/main/java/com/codingstudio/bookcatalog/asset/StoryAdapter.java
@@ -1,6 +1,7 @@
 package com.codingstudio.bookcatalog.asset;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +10,7 @@ import android.widget.ImageView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.codingstudio.bookcatalog.DetailActivity;
 import com.codingstudio.bookcatalog.R;
 
 import java.util.List;
@@ -33,6 +35,10 @@ public class StoryAdapter  extends RecyclerView.Adapter<StoryAdapter.ViewHolder>
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         holder.imgCover.setImageResource(images.get(position));
+        holder.itemView.setOnClickListener(v -> {
+            Intent intent = new Intent(context, DetailActivity.class);
+            context.startActivity(intent);
+        });
     }
 
     @Override
